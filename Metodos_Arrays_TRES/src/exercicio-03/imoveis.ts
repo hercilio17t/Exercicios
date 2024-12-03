3f01b30dcef8946a7ce7f8fe656f13585c86dedb
hercilio17t <136920405+hercilio17t@users.noreply.github.com>
2024-12-03 15:54:28 -0300

    Exercício 3

--- Metodos_Arrays_TRES/src/exercicio-03/imoveis.ts
+++ Metodos_Arrays_TRES/src/exercicio-03/imoveis.ts
@@ -1,3 +1,19 @@
+/*
+Problema
+Você trabalha para uma empresa de aluguel de imóveis e precisa de um sistema que permita ao 
+usuário listar os anúncios em ordem crescente de acordo com o valor do aluguel (do mais barato ao mais caro).
+
+Entrada
+A entrada será composta por uma variável:
+
+anuncios: array de objetos. Cada objeto armazena as propriedades titulo, descricao e preco de um imóvel anunciado
+Saída
+Seu programa deve RETORNAR:
+
+NAO ENCONTRADO: caso a lista de imóveis esteja vazia
+A lista de imóveis em ordem crescente de acordo com o valor do aluguel
+*/
+
 type TAnuncio = {
   titulo: string;
   descricao: string;
@@ -6,6 +22,11 @@ type TAnuncio = {
 
 function solucao(anuncios: TAnuncio[]): TAnuncio[] | string {
   // seu código aqui
+  if(anuncios.length === 0){
+    return 'NAO ENCONTRADO'
+  }
+
+  return anuncios.sort((a, b) => a.preco - b.preco)
   
 }
 export default solucao;
