87e90f0ed3efe6a0e2e8c1e9556f23c78cfd86c3
hercilio17t <136920405+hercilio17t@users.noreply.github.com>
2024-12-03 15:34:17 -0300

    Exercício 1

--- Metodos_Arrays_TRES/src/exercicio-01/ordenacaoPrecos.ts
+++ Metodos_Arrays_TRES/src/exercicio-01/ordenacaoPrecos.ts
@@ -1,7 +1,28 @@
+/*
+Problema
+Você está desenvolvendo a funcionalidade de um site responsável por filtrar os produtos por preço.
+
+Entrada
+A entrada será composta por duas variáveis:
+
+precos: array de number contendo os preços dos produtos anunciados pelo site
+filtroPreco: string que mostra o tipo de filtro que o usuário deseja. Os possíveis valores são 
+"crescente" ou "decrescente", caso o usuário deseje, respectivamente, ordenar os preços de forma crescente ou decrescente.
+Saída
+Seu programa deve RETORNAR a lista de preços ordenada.
+*/
+
 type TFiltroPreco = "crescente" | "decrescente";
 
 function solucao(precos: number[], filtroPreco: TFiltroPreco): number[] {
   // seu código aqui
-  
+  if(filtroPreco === 'crescente'){
+    return precos.sort((a, b) => a - b)
+  }
+  else if(filtroPreco === 'decrescente'){
+    return precos.sort((a, b) => b - a)
+  }
+  return precos
+
 }
 export default solucao;
