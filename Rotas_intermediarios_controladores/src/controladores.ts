32b3a232887c6639ec805377878da9773d93ebea
hercilio17t <136920405+hercilio17t@users.noreply.github.com>
2024-12-09 10:26:17 -0300

    Retornando convidadoID ou mensagem

--- Rotas_intermediarios_controladores/src/controladores.ts
+++ Rotas_intermediarios_controladores/src/controladores.ts
@@ -1,6 +1,22 @@
-import { Request, Response } from "express";
+import { Request, RequestParamHandler, Response } from "express"
+import bancoDeDados from "../bancoDeDados"
 
 
 export const mensagemAPI = (req: Request, res: Response) => {
     res.send("API de lista de convidados")
 }
+
+export const convidadoID = (req: Request, res: Response) => {
+    const { id } = req.params
+    const IDConvidado = parseInt(id, 10)
+
+    const convidado = bancoDeDados.find(c => c.id === IDConvidado)
+
+    if(convidado){
+        res.send(convidado)
+    }
+    else{
+        res.send("Convidado não encontrado")
+    }
+}
+
--- Rotas_intermediarios_controladores/src/index.ts
+++ Rotas_intermediarios_controladores/src/index.ts
@@ -1,9 +1,11 @@
 import 'dotenv/config'
 import express from 'express'
-import { mensagemAPI } from './controladores'
+import { convidadoID, mensagemAPI } from './controladores'
 
 const servidor = express()
 
 servidor.get('/', mensagemAPI)
+
+servidor.get('/convidados/:id', convidadoID)
 
 servidor.listen(process.env.PORT)
